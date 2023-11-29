7ba9ea1d30c4779f81f67e567429ee20cc55e543
Marian Amiragov <marian.amiragov@pm.me>
2023-11-29 16:34:00 +0100

    fix: fix wavdio-mirror not updating on build

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 rm -rf ./wavdio-angular
 rm -rf ./wavdio-express
+rm -rf ./wavdio-mirror
 git clone -b feat/containerize git@github.com:Brandmonks/wavdio-angular.git
 git clone -b feat/containerize git@github.com:Brandmonks/wavdio-express.git
 git clone git@github.com:Brandmonks/wavdio-mirror.git
